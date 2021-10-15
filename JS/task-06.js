be08dbd6bb97f274742b9b0ea86a4e199bc24b33
Dmytro Demianenko <83134601+DmytroDemianenko@users.noreply.github.com>
2021-10-15 20:12:26 +0200

    Update task-06.js

--- JS/task-06.js
+++ JS/task-06.js
@@ -1,21 +1,28 @@
 const inputEl = document.querySelector("#validation-input");
-console.log(inputEl);
-function onInputFocus() {
-  // console.log(`Input take a Focus`);
-}
-
-inputEl.addEventListener("input", onInputFocus);
-function checkInput(event) {
-  if (input.value.length === data - length.textContent)
-    inputEl.classList.add = ".#validation-input.valid";
-}
 
 function onInputBlur() {
-  //   console.log(`Input has lost a Focus`);
+  console.log(`Input has lost a Focus`);
 }
 inputEl.addEventListener("input", onInputBlur);
 
-function onInputChange() {
-  console.log(`Input has changed`);
+function inputMarkupCheking() {
+  if (inputEl["data-length"] === inputEl.innerHTML) {
+    console.log("Ok");
+  }
 }
-inputEl.addEventListener("input", onInputChange);
+
+console.log(inputEl.innerHTML);
+// function onInputFocus() {
+//   // console.log(`Input take a Focus`);
+// }
+
+// inputEl.addEventListener("input", onInputFocus);
+// function checkInput(event) {
+//   if (input.value.length === data - length.textContent)
+//     inputEl.classList.add = ".#validation-input.valid";
+// }
+
+// function onInputChange() {
+//   // console.log(`Input has changed`);
+// }
+// input.addEventListener("input", onInputChange);
