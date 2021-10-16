e54f701a4b61d098ff3e10973eabe5c10c8fba8b
Dmytro Demianenko <83134601+DmytroDemianenko@users.noreply.github.com>
2021-10-16 23:21:28 +0200

    Update task-07.js

--- JS/task-07.js
+++ JS/task-07.js
@@ -3,6 +3,5 @@ const textEl = document.querySelector("#text");
 
 inputEl.addEventListener("input", onInputChange);
 function onInputChange(e) {
-  console.log(e.currentTarget.value);
   textEl.style.fontSize = e.currentTarget.value + "px";
 }
